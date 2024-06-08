5f9916c91405fa5dc2b73af0e46ced60a0f79eb6
sedatcnn <sedatcnr02@gmail.com>
2024-06-08 14:51:04 +0300

    ufak tefek

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:patrimonie/firebase_options.dart';
-import 'package:patrimonie/page/anasayfa.dart';
-import 'package:patrimonie/page/harita.dart';
 import 'package:patrimonie/page/navbar/nav_controller.dart';
-import 'package:patrimonie/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
